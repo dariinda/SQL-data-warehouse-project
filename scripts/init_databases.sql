5048e51ad25ca7f605d3815e3a2764a1e8808ddc
Abhishek Singh <116362411+dariinda@users.noreply.github.com>
2025-07-20 11:45:11 +0530

    Update init_databases.sql

--- scripts/init_databases.sql
+++ scripts/init_databases.sql
@@ -1,18 +1,39 @@
 /*
 ----------------------------------------------------------------------------------
---------------------- Create the Databases for each layer ------------------------
+------------------------------  Database Creation  -------------------------------
 ----------------------------------------------------------------------------------
-This script creates the databases for each of the layer 
+This script creates the database and schema for each of the layer :
 - bronze
 - silver
 - gold
 
-Warnings 
-Do not drop the databases.
-Proper backup needs to taken of this stage.
+Warnings :
+- Do not drop the database.
+- Proper backup needs to taken of this stage.
 ----------------------------------------------------------------------------------
 
 */
-create schema bronze;
-create schema silver;
-create schema gold;
+USE master;
+GO
+
+-- Drop and recreate the database 'DataWarehouse'
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN 
+      ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+      DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- create the database 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+GO
+USE Datawarehouse;
+GO
+
+-- Create Schemas 
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
