fee31701ff508911807403fb6e789c9ad49c1be0
Abhishek Singh <116362411+dariinda@users.noreply.github.com>
2025-08-03 11:33:51 +0530

    Update transformations.sql

--- scripts/silver_layer/erp_px_cat_g1v2/transformations.sql
+++ scripts/silver_layer/erp_px_cat_g1v2/transformations.sql
@@ -2,3 +2,13 @@
 
 -- It has very good data quality 
 -- no changes are made here
+
+-- checking the data quality with quereies
+-- check for unwanted space
+select * 
+from bronze.erp_px_cat_g1v2
+where cat != trim(cat) or subcat != trim(subcat) or maintenance != trim(maintenance) 
+
+-- checking for standarization and consistency 
+select distinct maintenance
+from bronze.erp_px_cat_g1v2
