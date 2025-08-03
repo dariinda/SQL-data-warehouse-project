54e8f0c1e61ec17348c84a1078b9017c999081d3
Abhishek Singh <116362411+dariinda@users.noreply.github.com>
2025-08-03 10:18:51 +0530

    Update insert_into_erp_cust_az12.sql

--- scripts/silver_layer/erp_cust_az12/insert_into_erp_cust_az12.sql
+++ scripts/silver_layer/erp_cust_az12/insert_into_erp_cust_az12.sql
@@ -14,14 +14,17 @@ insert into silver.erp_cust_az12(
 	gen
 )
 select 
-		case 
-			when cid like 'NAS%' then substring(cid, 4, len(cid))
-			else cid
-		end as cid,
-		bdate, 
-		case 
-			when upper(trim(gen)) in ('F', 'FEMALE') then 'Female'
-			when upper(trim(gen)) in ('M', 'MALE') then 'Male'
-			else 'n/a'
-		end as  gen
+	case 
+		when cid like 'NAS%' then substring(cid, 4, len(cid))
+		else cid
+	end as cid,
+	case
+		when bdate > GETDATE() then null
+		else bdate
+	end as bdate, --set future dates null
+	case 
+		when upper(trim(gen)) in ('F', 'FEMALE') then 'Female'
+		when upper(trim(gen)) in ('M', 'MALE') then 'Male'
+		else 'n/a'
+	end as  gen
 from bronze.erp_cust_az12;
